b4a42728ae2565248daaa9801d681a78282b9cf3
inizio <newman.kcchow@gmail.com>
2017-11-10 08:49:37 +0800

    Build docker image with template Dockerfile

--- src/index.js
+++ src/index.js
@@ -1,13 +1,72 @@
 // @flow
 'user strict'
 
-const path = require('path')
-const docker = require('dockerode')
+const fs = require('fs')
+const tar = require('tar-fs')
+const Docker = require('dockerode')
+const {cwd, resolve} = require('./utils')
 
-const target = process.cwd()
-const resolve = dir => path.join(__dirname, dir)
+type settingsType = {
+  name?: string,
+  type?: 'nodejs'
+}
 
-let settings: Object = {}
-try {
-  settings = require(path.join(target, 'thing.config.js'))
-} catch (e) {}
+function getSettings (): settingsType {
+  let settings: settingsType = {}
+  try {
+    settings = require(resolve('thing.config.js', cwd))
+  } catch (err) {}
+  return settings
+}
+
+function buildImage (settings: settingsType) {
+  const srcDockerfile: string = resolve(`../docker-images/${settings.type || 'nodejs'}/Dockerfile`)
+  const destDockerfile: string = resolve('./Dockerfile', cwd)
+  let useDefaultDockerfile: boolean = false
+
+  // Use template dockerfile if not exist
+  if (!fs.existsSync(destDockerfile)) {
+    useDefaultDockerfile = true
+    try {
+      // $FlowFixMe
+      fs.copyFileSync(srcDockerfile, destDockerfile)
+    } catch (err) {
+      console.error(err)
+      console.log('> Failed to use template Dockerfile')
+    }
+  }
+
+  console.log('> Start building image')
+  const docker = new Docker()
+
+  docker.buildImage(
+    tar.pack(resolve('./', cwd)),
+    {
+      // NOTE: dockerode does not seem to support custom dockerfile in options?
+      // f: resolve(srcDockerfile),
+      t: settings.name
+    },
+    (err, output) => {
+      if (err) {
+        return console.error(err)
+      }
+      output.pipe(process.stdout, {end: true})
+      output.on('end', () => {
+        // Delete tempalte dockerfile
+        if (useDefaultDockerfile) {
+          fs.unlinkSync(destDockerfile)
+        }
+        imageDidBuild()
+      })
+    }
+  )
+}
+
+function imageDidBuild () {
+  console.log('> Finished building image')
+  // TODO: Push to docker registry
+}
+
+// Main
+const settings: settingsType = getSettings()
+buildImage(settings)
