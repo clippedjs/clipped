49bfa6c1a4b159d4628d4411dffddcff47c5bf1c
inizio <newman.kcchow@gmail.com>
2017-11-17 16:04:20 +0800

    File manipulations into promise

---
+++ src/utils/file-manipulation.js
@@ -0,0 +1,27 @@
+const {ncp} = require('graceful-ncp')
+const rimraf = require('rimraf')
+const mkdirp = require('mkdirp')
+
+module.exports.ncp = (src: string, dest: string, opts: Object = {}): Promise<void> =>
+  new Promise((resolve, reject) => {
+    ncp(src, dest, opts, err => {
+      if (err) reject(err)
+      resolve()
+    })
+  })
+
+module.exports.rimraf = (f: string, opts: Object = {}): Promise<void> =>
+  new Promise((resolve, reject) => {
+    rimraf(f, opts, err => {
+      if (err) reject(err)
+      resolve()
+    })
+  })
+
+module.exports.mkdirp = (path: string, opts: Object = {}): Promise<void> =>
+  new Promise((resolve, reject) => {
+    mkdirp(path, opts, err => {
+      if (err) reject(err)
+      resolve()
+    })
+  })
