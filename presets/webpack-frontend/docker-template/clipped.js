a55298d89b1a6d29fff1259f4c82c44f7c47c245
inizio <newman.kcchow@gmail.com>
2018-01-02 13:35:41 +0800

    fix(Error handler for frontend server):

--- presets/webpack-frontend/docker-template/clipped.js
+++ presets/webpack-frontend/docker-template/clipped.js
@@ -1,11 +1,18 @@
 const server = require('server')
+const {render} = server.reply
+const {error} = server.router
+const {status} = server.reply
 
 const PORT = process.env.PORT || 8080
 
-server({
-  public: './',
-  views: './',
-  port: PORT
-}, ctx => render('index.html'))
+server(
+  {
+    public: './',
+    views: './',
+    port: PORT
+  },
+  ctx => render('index.html'),
+  error(ctx => status(500).send(ctx.error.message))
+)
 
 console.log(`> Server listening at port ${PORT}`)
