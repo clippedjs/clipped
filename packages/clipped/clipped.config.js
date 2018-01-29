125fd502d23c6b936c2cab8171b66452135ee8e6
inizio <digit4free@gmail.com>
2018-01-29 14:37:16 +0800

    build(jointed): Use new jointed tool for building itself

--- packages/clipped/clipped.config.js
+++ packages/clipped/clipped.config.js
@@ -5,69 +5,65 @@ const webpack = require('webpack')
 const nodeExternals = require('webpack-node-externals')
 
 module.exports = async clipped => {
-  await clipped.use(require('clipped-preset-webpack-nodejs'))
-
-  clipped.config.webpack.merge({
-    resolve: {
-      symlinks: true
-    },
-    resolveLoader: {
-      symlinks: true
-    }
-  })
-
-  clipped.config.webpack.externals([nodeExternals({
-    modulesFromFile: true,
-    whitelist: [/^webpack/]
-  })])
+  await clipped.use(require('clip-webpack-nodejs'))
 
   clipped.config.webpack
-    .plugin('banner')
-    .tap(args => {
-      args[0].banner = `require('source-map-support/register')`
-      return args
-    })
-
-  clipped.config.webpack.module
-    .rule('shebang')
-    .test( /node_modules[/\\]jsonstream/i)
-    .use('shebang').loader(require.resolve('shebang-loader')).end()
+    .mark()
+      .resolve
+        .set('symlinks', true)
+    .back()
+      .resolveLoader
+        .set('symlinks', true)
+    .back()
+      .set('externals', [
+        nodeExternals({
+          modulesFromFile: true,
+          whitelist: [/^webpack/]
+        })
+      ])
 
   clipped.config.webpack
-    .plugin('ignore-electron')
-      .use(webpack.IgnorePlugin, [/^electron$/])
+    .plugins
+      .mark()
+        .banner
+          .modify('args', args => {
+            args[0].banner = "require('source-map-support/register')"
+            return args
+          })
 
-  clipped.config.webpack.resolve.modules
-    .add('node_modules')
-  clipped.config.webpack.resolveLoader.modules
-    .add('node_modules')
+  clipped.config.webpack
+    .mark()
+      .module
+        .rules
+          .set('shebang', {
+            test: /node_modules[/\\]jsonstream/i,
+            loader: require.resolve('shebang-loader')
+          })
+          .set('rx', {
+            test: /rx\.lite\.aggregates\.js/,
+            loader: `${require.resolve('imports-loader')}?define=>false`
+          })
+    .back()
+      .plugins
+        .use('ignore-electron', webpack.IgnorePlugin, [/^electron$/])
 
-  clipped.config.webpack.module
-    .rule('rx')
-    .test(/rx\.lite\.aggregates\.js/)
-    .use('import').loader(`${require.resolve('imports-loader')}?define=>false`).end()
+  if (process.env.NODE_ENV === 'test') {
+    clipped.config.webpack
+      .mark()
+        .entry
+          .set('index', glob.sync('./test/**/*.test.js'))
+      .back()
+        .output
+          .set('path', clipped.resolve('test-dist'))
 
-  // Compile for testing
-  clipped.config.webpack.when(process.env.NODE_ENV === 'test', config => {
-    config.entry('index')
-      .clear()
-      .end()
-
-    config.output
-      .path(clipped.resolve('test-dist'))
-
-    for (let entry of glob.sync('./test/**/*.test.js')) {
-      config.entry('index')
-        .add(entry)
-        .end()
-    }
-
-    config.module
-      .rule('babel')
-      .test(/\.js$/)
-        .include
-        .add(clipped.resolve('test'))
-  })
+    
+    clipped.config.webpack
+      .module
+        .rules
+          .babel
+            .include
+              .add(clipped.resolve('test'))
+  }
 
   clipped.hook('pre-test')
     .add('build', clipped => clipped.execHook('build'))
