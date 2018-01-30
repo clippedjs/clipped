8c0b55c7808071de0781cb138821191e31e8ec50
inizio <digit4free@gmail.com>
2018-01-30 21:43:25 +0800

    ...

--- packages/clipped/clipped.config.js
+++ packages/clipped/clipped.config.js
@@ -5,7 +5,7 @@ const webpack = require('webpack')
 const nodeExternals = require('webpack-node-externals')
 
 module.exports = async clipped => {
-  await clipped.use(require('@clipped/webpack-nodejs'))
+  await clipped.use(require('@clipped/preset-webpack-nodejs'))
 
   clipped.config.webpack
     .mark()
