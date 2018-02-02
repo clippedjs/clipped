a1427af879cd1edf6a27fb8a603e5b54dba86ef0
inizio <digit4free@gmail.com>
2018-02-02 18:46:16 +0800

    First working coverage

--- packages/clipped/clipped.config.js
+++ packages/clipped/clipped.config.js
@@ -45,24 +45,37 @@ module.exports = async clipped => {
     .back()
       .plugins
         .use('ignore-electron', webpack.IgnorePlugin, [/^electron$/])
+      
+        clipped.config.webpack.devtool = 'source-map'
 
-  // if (process.env.NODE_ENV === 'test') {
-  //   clipped.config.webpack
-  //     .mark()
-  //       .entry
-  //         .set('index', glob.sync('./test/**/*.test.js'))
-  //     .back()
-  //       .output
-  //         .set('path', clipped.resolve('test-dist'))
+  if (process.env.NODE_ENV === 'test') {
+    // clipped.config.webpack
+    //   .mark()
+    //     .entry
+    //       .set('index', glob.sync('./test/**/*.test.js'))
+    //   .back()
+    //     .output
+    //       .set('path', clipped.resolve('test-dist'))
 
     
-  //   clipped.config.webpack
-  //     .module
-  //       .rules
-  //         .babel
-  //           .include
-  //             .add(clipped.resolve('test'))
-  // }
+    // clipped.config.webpack
+    //   .module
+    //     .rules
+    //       .babel
+    //         .include
+    //           .add(clipped.resolve('test'))
+    // clipped.config.webpack.devtool = 'inline-source-map'
+
+    // clipped.config.webpack
+    //   .module
+    //       .rules
+    //         .babel
+    //           .use
+    //             .babel
+    //               .set('plugins', [
+    //                 [require.resolve('babel-plugin-istanbul')]
+    //               ])
+  }
 
   // clipped.hook('pre-test')
   //   .add('build', clipped => clipped.execHook('build'))
