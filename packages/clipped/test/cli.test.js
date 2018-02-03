f2d0542e42e02e813effe954447aa819bafda10f
inizio <digit4free@gmail.com>
2018-02-03 11:20:19 +0800

    Try init

--- packages/clipped/test/cli.test.js
+++ packages/clipped/test/cli.test.js
@@ -2,18 +2,26 @@ const path = require('path')
 const assert = require('assert')
 const {default: Clipped, cli: clippedCli} = require('../dist')
 
-// describe('clippedCli', async function () {
-//   describe('#parameterless', function () {
-//     it('should return help when no parameters given', async function () {
-//         const response = await clippedCli()
-//     })
-//   })
+describe('clippedCli', async function () {
+  describe('#parameterless', function () {
+    it('should return help when no parameters given', async function () {
+        const response = await clippedCli()
 
-//   describe('#action', function () {
-//     it('should run action when given action', async function () {
-//       const response = await clippedCli({action: 'version'})
+        assert.ok(response)
+    })
+  })
 
-//       assert.ok(response)
-//     })
-//   })
-// })
+  describe('#action', function () {
+    it('should run action when given action', async function () {
+      const response = await clippedCli({action: 'version'})
+
+      assert.ok(response)
+    })
+  })
+
+  describe('#init', function () {
+    const response = clippedCli({action: 'init'})
+
+    assert.ok(response)
+  })
+})
