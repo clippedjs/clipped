5d78aa54aaabd1dd1ae217652eae74982e12bc43
inizio <newman.kcchow@gmail.com>
2018-04-29 23:18:55 +0800

    feat(preset-webpack4): backend preset

---
+++ presets/webpack4/backend/clipped.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const nodemon = require('nodemon')
+
+module.exports = async clipped => {
+  await clipped.use(require('@clipped/preset-webpack4-library'))
+
+  let mainFile = path.join(clipped.config.context, clipped.config.packageJson.main)
+  try {
+    require(mainFile)
+  } catch (e) {
+    mainFile = null
+  }
+
+  clipped.hook('dev')
+    .add('watch and nodemon', [
+      clipped => clipped.execHook('watch'),
+      clipped => new Promise((resolve, reject) => {
+        nodemon({
+          // Main field in package.json or dist/index.js
+          script: mainFile || clipped.config.main || path.join(clipped.config.dist, 'index.js'),
+          ext: 'js json',
+          ignore: ["test/*", "docs/*"]
+        })
+
+        nodemon.on('start', function () {
+          console.log('> App has started')
+        }).on('quit', function () {
+          console.log('> App has quit')
+          process.exit();
+        }).on('restart', function (files) {
+          console.log('> App restarted due to: ', files)
+        })
+      })
+    ])
+}
