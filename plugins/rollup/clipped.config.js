a362d8309f51ce1b041a3d11c83a4dcb7537497f
inizio <newman.kcchow@gmail.com>
2018-11-06 23:16:24 +0800

    ✨ adds useful plugins for rollup

--- plugins/rollup/clipped.config.js
+++ plugins/rollup/clipped.config.js
@@ -1,21 +1,24 @@
 const path = require('path')
 const rollup = require('rollup')
+// const {uglify} = require('rollup-plugin-uglify')
+// const {minify} = require('uglify-es')
 
-module.exports = ({type = 'library', babel = {}, formats = ['cjs']} = {}) => [
+module.exports = ({name = null, type = 'library', babel = {}, formats = ['cjs'], commonjs = {}, postcss = {}, globals = {}, external = [], moduleName = '', alias = {}, replace = {}} = {}) => [
   babel && require('@clipped/plugin-babel')({type, ...babel}),
   {
     rollup: {
-      external: [],
+      external,
       plugins: []
     }
   },
   {
     rollup(cfg, {config}) {
-      cfg.input = config.src
+      cfg.input = path.resolve(config.src, 'index.js')
       cfg.output = formats.map(format => ({
-        file: path.resolve(config.dist, `index.${format}.js`),
+        name: moduleName,
+        file: path.resolve(config.dist, `${name || config.packageJson.name}.${format}.js`),
         format,
-        globals: {},
+        globals,
         sourcemap: true
       }))
 
@@ -24,6 +27,32 @@ module.exports = ({type = 'library', babel = {}, formats = ['cjs']} = {}) => [
         cfg.plugins
           .use('babel', require('rollup-plugin-babel'), [{}])
           .alias('args.0', () => config.babel)
+      }
+
+      if (postcss) {
+        cfg.plugins
+          .use('postcss', require('rollup-plugin-postcss'), [{
+            extract: true,
+            minimize: process.env.NODE_ENV === 'production',
+            sourceMap: true,
+            ...postcss
+          }])
+      }
+
+      if (commonjs) {
+        cfg.plugins.use('commonjs', require('rollup-plugin-commonjs'), [commonjs])
+      }
+
+      if (Object.keys(alias).length > 0) {
+        cfg.plugins.use('alias', require('rollup-plugin-alias'), [alias])
+      }
+
+      if (Object.keys(replace).length > 0) {
+        cfg.plugins.push(require('rollup-plugin-replace')(replace).transform)
+      }
+
+      if (process.env.NODE_ENV === 'development') {
+        cfg.plugins.use('progress', require('rollup-plugin-progress'), [{}])
       }
 
       // if (['umd', 'iife'].includes(cfg.output.format)) {
@@ -39,15 +68,52 @@ module.exports = ({type = 'library', babel = {}, formats = ['cjs']} = {}) => [
       // }
 
       if (process.env.NODE_ENV !== 'development') {
-        cfg.plugins.use('uglify', require('rollup-plugin-uglify').uglify, [{}, require('uglify-es').minify])
+        if (babel) {
+          cfg.plugins.use('babel-minify', require('rollup-plugin-babel-minify'), [{}])
+        } else {
+          cfg.plugins.use('uglify', uglify, [{}, minify])
+        }
       }
+
+      cfg.plugins.use('multi-entry', require('rollup-plugin-multi-entry'), [])
     }
   },
   api => {
     api.hook('build')
       .add('rollup', async api => {
-        const bundle = await rollup.rollup(api.config.rollup.toJSON())
-        await bundle.write(config.toConfig().output)
+        const bundle = await rollup.rollup(api.config.rollup.toConfig())
+        await Promise.all([].concat(api.config.rollup.toConfig().output).map(bundle.write))
+      })
+
+    api.hook('watch')
+      .add('rollup', async api => {
+        if (!api.config.rollup.plugins.has('progress')) {
+          api.config.rollup.plugins.use('progress', require('rollup-plugin-progress'), [{}])
+        }
+
+        return new Promise((resolve, reject) => {
+          const watcher = rollup.watch({
+            ...api.config.rollup.toConfig(),
+            watch: {
+              clearScreen: true
+            }
+          })
+          watcher.on('event', async e => {
+            switch (e.code) {
+              case 'BUNDLE_START':
+                api.print('🚀  Watch mode started')
+                break
+              case 'ERROR':
+              case 'FATAL':
+                reject(e.error)
+                break
+              case 'BUNDLE_END':
+                process.exitCode = 0
+                api.print('Finished bundling, waiting for file change...')
+                break                
+            }
+          })
+        })
       })
   }
 ]
