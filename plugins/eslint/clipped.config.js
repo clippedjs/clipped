b720634d4c48e63b6cb050fe59527f9ed099d527
inizio <newman.kcchow@gmail.com>
2018-11-11 11:38:37 +0800

    feat(eslint): adds xo mode for eslint plugin

--- plugins/eslint/clipped.config.js
+++ plugins/eslint/clipped.config.js
@@ -1,19 +1,28 @@
 const {CLIEngine} = require('eslint')
 const {rules: standardRules} = require('eslint-config-standard')
 
-module.exports = ({babel = false, fix = true, useEslintrc = false, format = 'codeframe', test = /\.(j|t)sx?$/, standardjs = true} = {}) => [
+module.exports = ({
+  babel = false,
+  fix = true,
+  useEslintrc = false,
+  format = 'codeframe',
+  extensions = ['vue', 'js', 'ts', 'tsx', 'jsx', 'marko', 'radi'],
+  mode = 'xo'
+} = {}) => [
   {
     eslint: {
       globals: [],
       plugins: [],
       extends: [],
-      rules: {},
+      rules: {
+        semi: 'off'
+      },
       overrides: []
     },
     webpack(cfg, api) {
       cfg.module.rules.set('eslint', {
         enforce: 'pre',
-        test,
+        test: new RegExp(`.(${extensions.join('|')})$`),
         exclude: /node_modules/,
         loader: require.resolve('eslint-loader')
       })
@@ -21,8 +30,15 @@ module.exports = ({babel = false, fix = true, useEslintrc = false, format = 'cod
       cfg.module.rules.eslint.alias('options', () => api.config.eslint)
     }
   },
-  standardjs && {
-    eslint (cfg) {
+  mode === 'xo' && {
+    eslint(cfg) {
+      cfg.space = true
+      cfg.semicolon = false
+      cfg.prettier = true
+    }
+  },
+  mode === 'standardjs' && {
+    eslint(cfg) {
       Object.assign(cfg, {
         extends: [
           require.resolve('eslint-config-standard'),
@@ -33,24 +49,22 @@ module.exports = ({babel = false, fix = true, useEslintrc = false, format = 'cod
         // ...so we have no choice but to rely on it being hoisted.
         plugins: ['standard'],
         rules: {
-          // Disable rules for which there are eslint-plugin-babel replacements:
-          // https://github.com/babel/eslint-plugin-babel#rules
-          'new-cap': 'off',
-          'no-invalid-this': 'off',
-          'object-curly-spacing': 'off',
-          'semi': 'off',
-          'no-unused-expressions': 'off',
-          ...(!babel ? {} : {
-            // Ensure the replacement rules use the options set by eslint-config-standard rather than ESLint defaults.
-            'babel/new-cap': standardRules['new-cap'],
-            // eslint-config-standard doesn't currently have an explicit value for these two rules, so
-            // they default to off. The fallbacks are not added to the other rules, so changes in the
-            // preset configuration layout doesn't silently cause rules to be disabled.
-            'babel/no-invalid-this': standardRules['no-invalid-this'] || 'off',
-            'babel/object-curly-spacing': standardRules['object-curly-spacing'] || 'off',
-            'babel/semi': standardRules.semi,
-            'babel/no-unused-expressions': standardRules['no-unused-expressions']
-          })
+          ...(babel
+            ? {
+                // Ensure the replacement rules use the options set by eslint-config-standard rather than ESLint defaults.
+                'babel/new-cap': standardRules['new-cap'],
+                // eslint-config-standard doesn't currently have an explicit value for these two rules, so
+                // they default to off. The fallbacks are not added to the other rules, so changes in the
+                // preset configuration layout doesn't silently cause rules to be disabled.
+                'babel/no-invalid-this':
+                  standardRules['no-invalid-this'] || 'off',
+                'babel/object-curly-spacing':
+                  standardRules['object-curly-spacing'] || 'off',
+                'babel/semi': standardRules.semi,
+                'babel/no-unused-expressions':
+                  standardRules['no-unused-expressions']
+              }
+            : {})
         },
         settings: {
           react: {
@@ -62,28 +76,57 @@ module.exports = ({babel = false, fix = true, useEslintrc = false, format = 'cod
       })
     }
   },
-  api => {
-    api.hook('lint')
-      .add('eslint', api => {
-        const engine = new CLIEngine({
-          fix,
-          root: true,
-          env: {node: true},
-          baseConfig: useEslintrc ? undefined : api.config.eslint.toConfig(),
-          useEslintrc
+  mode === 'xo'
+    ? api => {
+        api.hook('lint').add('xo', async api => {
+          const xo = require('xo')
+
+          const report = await xo.lintFiles([], {
+            ...api.config.eslint.toConfig(),
+            extensions,
+            fix
+          })
+          if (fix) {
+            xo.outputFixes(report)
+          }
+          const formatter = xo.getFormatter(format)
+
+          if (report.errorCount || report.warningCount) {
+            api.print(formatter(report.results))
+          } else {
+            api.print(
+              report.results.some(f => f.output)
+                ? '🎉  All lint errors auto-fixed'
+                : '🎉  No lint errors found'
+            )
+          }
         })
-        const formatter = engine.getFormatter(format)
-        const report = engine.executeOnFiles([api.config.context])
+      }
+    : api => {
+        api.hook('lint').add('eslint', api => {
+          const engine = new CLIEngine({
+            fix,
+            root: true,
+            env: {node: true},
+            baseConfig: useEslintrc ? undefined : api.config.eslint.toConfig(),
+            useEslintrc
+          })
+          const formatter = engine.getFormatter(format)
+          const report = engine.executeOnFiles([api.config.context])
 
-        if (fix) {
-          CLIEngine.outputFixes(report)
-        }
+          if (fix) {
+            CLIEngine.outputFixes(report)
+          }
 
-        if (report.errorCount || report.warningCount) {
-          api.print(formatter(report.results))
-        } else {
-          api.print(report.results.some(f => f.output) ? 'All lint errors auto-fixed' : 'No lint errors found')
-        }
-      })
-  }
+          if (report.errorCount || report.warningCount) {
+            api.print(formatter(report.results))
+          } else {
+            api.print(
+              report.results.some(f => f.output)
+                ? '🎉  All lint errors auto-fixed'
+                : '🎉  No lint errors found'
+            )
+          }
+        })
+      }
 ]
