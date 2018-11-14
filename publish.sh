9b3c537a69b48b4f608c4e1c8a323f7740b59c21
inizio <newman.kcchow@gmail.com>
2018-11-14 14:42:19 +0800

    refactor(deps): slim down deps

--- publish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# for d in presets/*; do autopublish "$d"; done
-# for d in generators/*; do autopublish "$d"; done
-# lerna run semantic-release
-
-# lerna publish --conventional-commits --yes
