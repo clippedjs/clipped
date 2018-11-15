888554638f950984eabcd0c01dcc13d6a03e31c8
inizio <newman.kcchow@gmail.com>
2018-11-15 23:17:45 +0800

    feat(mdx): adds mdx plugin

---
+++ plugins/mdx/clipped.config.js
@@ -0,0 +1,25 @@
+module.exports = () => [
+  {
+    webpack(cfg) {
+      cfg.module.rules.use('mdx', {
+        test: /\.mdx?$/,
+        include: [api.config.src],
+        exclude: /(node_modules|bower_components)/,
+        use: [{
+          key: 'babel',
+          value: {
+            loader: require.resolve('babel-loader'),
+            options: {}
+          }
+        },{
+          key: 'mdx',
+          value: {
+            loader: require.resolve('mdx-loader'),
+            options: {}
+          }
+        }]
+      })
+      cfg['module.rules.mdx.use.babel'].alias('options', () => api.config.babel)
+    }
+  }
+]
